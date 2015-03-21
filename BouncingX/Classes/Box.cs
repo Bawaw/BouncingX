784bc9d9aef243571f1873b17bfd01344e34bbeb
Bawaw <gaming.anon@yandex.com>
2015-03-21 20:18:14 +0100

    Visual console - 2012

---
+++ BouncingX/Classes/Box.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace BouncingX
+{
+    class Box
+    {
+        const int HEALTH = 100;
+        const int DAMAGE = 10;
+
+        private Point position;
+        private Point velocity;
+        private List<ConsoleColor> colors;
+        private int character;
+        private int health = HEALTH;
+
+        # region properties
+        public Point Position 
+        { 
+            get 
+            { 
+                return position; 
+            } 
+            set 
+            { 
+                position = value; 
+            } 
+        }
+
+        public Point Velocity 
+        {
+            get 
+            {
+                return velocity; 
+            }
+            set 
+            {
+                velocity = value; 
+            } 
+        }
+
+        public int Character
+        {
+            get
+            {
+                return character;
+            }
+            set
+            {
+                character = value;
+            }
+        }
+
+        public int Health { get { return health; }}
+        
+        #endregion
+
+        #region methods
+
+        public void Initialize()
+        { 
+            colors = new List<ConsoleColor>();
+            colors.Add(ConsoleColor.Red);
+            colors.Add(ConsoleColor.Yellow);
+            colors.Add(ConsoleColor.Green);
+            colors.Add(ConsoleColor.White);
+        }
+
+        private void wallHitt()
+        {
+            TakeDamage();
+        }
+        public void TakeDamage()
+        {
+            if (health - DAMAGE > 0)
+                health -= DAMAGE;
+
+            else health = 0;
+
+        }
+
+
+        public void update()
+        {
+            position.X += velocity.X;
+            position.Y += velocity.Y;
+
+            checkWallCollision();
+        }
+
+       public void draw ()
+       {
+           float percent = (float)health / HEALTH;
+           int ColorIndex = (int)(percent * colors.Count);
+           ColorIndex = Math.Min(ColorIndex, colors.Count -1 );
+
+           character = health;
+           Console.ForegroundColor = colors[ColorIndex];
+
+           Console.SetCursorPosition(position.X, position.Y);
+           Console.Write(this.character);
+
+       }
+       
+       private void checkWallCollision()
+       {
+           string convert = Health.ToString();
+           int charWidth = convert.Length;
+
+           if (Position.X < 0)
+           {
+               position.X = 0;
+               velocity.X *= -1;
+               wallHitt();
+           }
+           else if (position.X > Console.WindowWidth - charWidth)
+           {
+               position.X = Console.WindowWidth - charWidth;
+               velocity.X *= -1;
+               wallHitt();
+
+           }
+
+           if (Position.Y < 0)
+           {
+               position.Y = 0;
+               velocity.Y *= -1;
+               wallHitt();
+           }
+           else if (position.Y > Console.WindowHeight- 1)
+           {
+               position.Y = Console.WindowHeight - 1;
+               velocity.Y *= -1;
+               wallHitt();
+           }
+       }
+       #endregion
+    }
+}
---
+++ BouncingX/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace BouncingX
+{
+    class Program
+    {
+        static List<Box> boxes;
+        static Random random;
+
+        static void Main(string[] args)
+        {
+            boxes = new List<Box>();
+            random = new Random();
+            
+            Box newBox;
+            newBox = new Box();
+            newBox.Initialize();
+            newBox.Velocity = new Point(1, 1);
+            newBox.Character = newBox.Health;
+            
+            boxes.Add(newBox);
+
+            while(true)
+            {
+                Console.Clear();
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    if (keyInfo.Key == ConsoleKey.Spacebar)
+                    {
+                        AddRandomBox(ConsoleColor.Blue);
+                    }
+                }
+                
+
+                for (int i = boxes.Count -1 ; i >= 0; i--)
+                {
+                    boxes[i].update();
+                    if (boxes[i].Health > 0)
+                    {            
+                           boxes[i].draw();
+                    }
+
+                    else
+                    {
+                           boxes.RemoveAt(i);
+                           continue;
+                    }
+
+                                      
+                }
+
+                System.Threading.Thread.Sleep(33);
+            }
+        }
+
+        static void AddRandomBox(ConsoleColor color)
+        {
+            Box box = new Box();
+            box.Initialize();
+            box.Velocity = new Point(random.Next(1, 4), random.Next(1, 4));
+            box.Position = new Point(random.Next(Console.WindowWidth), random.Next(Console.WindowHeight));
+            box.Character = box.Health;
+            boxes.Add(box);
+        }
+    }
+}
